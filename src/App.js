9c63a0a8bf0aeddfbd83eba271cc7daa0d1f0f27
Daniel Parsons <danielparsons3133382@gmail.com>
2019-09-29 22:11:27 +1000

    Complete app with styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -15,10 +17,23 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
   render() {
+    const { monsters, searchField } = this.state; 
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
     return (
-      <div className='App'>
-        { this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>) }
+      <div className='app'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={filteredMonsters} />
       </div> 
     );
   }
